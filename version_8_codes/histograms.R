840080ab64ad84e8a901f6a6d310ce09a0afc978
ToniErtl <ertl.toni@gmail.com>
2024-02-21 22:21:26 +0100

    added histograms

--- version_8_codes/histograms.R
+++ version_8_codes/histograms.R
@@ -1,3 +1,8 @@
+# Histograms
+
+rm(list=ls())
+
+
 library(tidyverse)
 
 data <- read.dta(".//chowdhurry data//Data Archive//ConstructedData//children_familyAggregate_stat12.dta",
@@ -64,12 +69,62 @@ for ( col in 1:ncol(data)){
 data <- data.frame(lapply(data, function(x) type.convert(as.character(x), as.is = TRUE)))
 
 
+data_cont <-data.frame(data$patient_choicesOffspringMean,
+                       data$patient_choices_father,
+                       data$patient_choices_mother,
+                       data$binswangerOffspringMean,
+                       data$binswanger_father,
+                       data$binswanger_mother
+) 
 
 
 
 
 
 
+p1 <- ggplot(data, aes(x = patient_choicesOffspringMean)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Children (Mean)", x = "Number of Patient Choices", y = "Density") +
+  theme_minimal()
+
+# Histogram for patient_choices_father
+p2 <- ggplot(data, aes(x = patient_choices_father)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Father", x = "Number of Patient Choices", y = "Density") +
+  theme_minimal()
+
+# Histogram for patient_choices_mother
+p3 <- ggplot(data, aes(x = patient_choices_mother)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Mother", x = "Number of Patient Choices", y = "Density") +
+  theme_minimal()
+
+
+ggsave("./comment_clustering_plots/hist_patience.pdf",
+       ggpubr::ggarrange(p1, p2, p3, ncol = 3, nrow = 1),
+       width = 12, height = 4)
+
+
+p4 <- ggplot(data, aes(x = binswangerOffspringMean)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Children (Mean)", x = "Gamble Number picked", y = "Density") +
+  theme_minimal()
+
+p5 <- ggplot(data, aes(x = binswanger_father)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Father", x = "Gamble Number picked", y = "Density") +
+  theme_minimal()
+
+
+p6 <- ggplot(data, aes(x = binswanger_mother)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "midnightblue", color = "black") +
+  labs(title = "Mother", x = "Gamble Number picked", y = "Density") +
+  theme_minimal()
+
+
+ggsave("./comment_clustering_plots/hist_risk.pdf",
+       ggpubr::ggarrange(p4, p5, p6, ncol = 3, nrow = 1),
+       width = 12, height = 4)
 
 
 
